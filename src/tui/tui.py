dff1cf16f8b5994c356931c8bd8f53c575f597e5
Simon <simon21.blum@gmail.com>
2024-07-30 10:24:54 +0200

    Add basics for too small terminal

--- src/tui/tui.py
+++ src/tui/tui.py
@@ -29,6 +29,27 @@ def start_tui(stdscr: CursesWindow) -> None:
     window: Window = init_tui(stdscr)
     connection, cursor = connect_to_db("test.db")
 
+    height, width = window.getSize()
+    window().refresh()
+
+    to_small = False
+    if height < 30 or width < 80:
+        to_small = True
+
+    while to_small:
+        # height_2, width_2 = window.getSize()
+        # if height_2 != height or width_2 != width:
+        height, width = window.getSize()
+        window().clear()
+        window.writeCenteredText(f"Your terminal size {width}x{height} is too small")
+        window().refresh()
+        if height >= 30 and width >= 80:
+            to_small = False
+
+        time.sleep(0.1)
+
+    window().clear()
+
     # choice = show_start(window)
     # window().clear()
     # if choice == 1:
