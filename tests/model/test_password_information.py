c7cb9d03524dd22cb9f5d14ab89c7f2a2b96c41a
Simon <simon21.blum@gmail.com>
2024-08-07 22:24:08 +0200

    Hopefully fix tests take 2

--- tests/model/test_password_information.py
+++ tests/model/test_password_information.py
@@ -11,16 +11,16 @@ from src.model.user import User
 
 class TestPasswordInformation(unittest.TestCase):
     def test_init(self):
-        info, pw, user = create_test_info()
+        _ = create_test_info()
 
     def test_note(self):
-        info, pw, user = create_test_info()
+        info, _, _ = create_test_info()
         note = "This password is a test"
         info.set_note(note)
         self.assertEqual(info.details.note, note)
 
     def test_category_maximum(self):
-        info, pw, user = create_test_info()
+        info, _, _ = create_test_info()
         categories = ["Test", "Old", "Password"]
         info.add_categories(categories)
         more_categories = ["AnotherTest"], ["Safe"], ["Uncool"]
@@ -30,36 +30,38 @@ class TestPasswordInformation(unittest.TestCase):
         self.assertRaises(ValueError, info.add_category, "Uncool")
 
     def test_duplicate_category(self):
-        info, pw, user = create_test_info()
+        info, _, _ = create_test_info()
         info.add_category("Test")
         self.assertRaises(ValueError, info.add_category, "Test")
 
     def test_encrypt(self):
-        info, pw, user = create_test_info()
+        info, _, _ = create_test_info()
         info.add_password(Password("test2"))
         for password in info.passwords:
             self.assertFalse(password.is_encrypted)
-        self.assertTrue(isinstance(info.metadata, Metadata))
+        self.assertIsInstance(info.metadata, Metadata)
 
-        info.encrypt("FakeKey")
+        info.encrypt_data(key="FakeKey")
+        info.encrypt_passwords(key="FakeKey")
         for password in info.passwords:
             self.assertTrue(password.is_encrypted)
-        self.assertTrue(isinstance(info.metadata, EncryptedMetadata))
+        self.assertIsInstance(info.metadata, EncryptedMetadata)
 
     def test_decrypt(self):
-        info, pw, user = create_test_info()
+        info, _, _ = create_test_info()
 
         info.add_password(Password("test2"))
-        info.encrypt("FakeKey")
+        info.encrypt_data(key="FakeKey")
+        info.encrypt_passwords(key="FakeKey")
         for password in info.passwords:
             self.assertTrue(password.is_encrypted)
-        self.assertTrue(isinstance(info.metadata, EncryptedMetadata))
+        self.assertIsInstance(info.metadata, EncryptedMetadata)
 
-        info.decrypt("FakeKey")
+        info.decrypt_data(key="FakeKey")
+        info.decrypt_passwords(key="FakeKey")
         for password in info.passwords:
             self.assertFalse(password.is_encrypted)
-        self.assertTrue(isinstance(info.metadata, Metadata))
-
+        self.assertIsInstance(info.metadata, Metadata)
 
 
 def create_test_info() -> tuple[PasswordInformation, Password, User]:
