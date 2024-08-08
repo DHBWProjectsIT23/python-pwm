2b0d587c5029f4cb1fe34f52e275b08083fb396a
maxrodler <maximilianreinerrodler@gmail.com>
2024-08-08 15:54:27 +0200

    fernet correction

--- src/crypto/fernet.py
+++ src/crypto/fernet.py
@@ -1,11 +1,14 @@
 from cryptography.fernet import Fernet
+import base64
 
 
 def encrypt_fernet(data: bytes, key: bytes) -> bytes:
-    f = Fernet(key)
+    base64_key = base64.urlsafe_b64encode(key)
+    f = Fernet(base64_key)
     return f.encrypt(data)
 
 
 def decrypt_fernet(data: bytes, key: bytes) -> bytes:
-    f = Fernet(key)
+    base64_key = base64.urlsafe_b64encode(key)
+    f = Fernet(base64_key)
     return f.decrypt(data)
